acc5057ad68f5dd841c6dab2e3373d238f2adb7d
hietalajulius <julius.hietala@gmail.com>
2022-03-08 16:44:37 +0200

    Add setup.py for docs

---
+++ setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="stray",
+    version="0.0.2",
+    author="Stray Robots",
+    author_email="hello@strayrobots.io",
+    description="Stray SDK",
+    url="https://strayrobots.io",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        'trimesh',
+        'scipy',
+        'pillow',
+        'numpy',
+        'scikit-video',
+        'pycocotools',
+        'open3d'
+    ],
+)
