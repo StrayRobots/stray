241c8be65c1b606837638f00723f5d2b893b5706
hietalajulius <julius.hietala@gmail.com>
2022-03-16 14:54:45 +0200

    Update versions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="stray",
-    version="0.0.6",
+    version="0.0.7",
     author="Stray Robots",
     author_email="hello@strayrobots.io",
     description="Stray SDK",
